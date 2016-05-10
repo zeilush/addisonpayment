8c3732ec016a2ee65bfef54a62b1e73d1703bcd5
Artur Zeiler <artur.zeiler@addison.de>
2016-05-10 16:08:01 +0200

    Initial Import

---
+++ src/main/java/de/wkss/addisonpayment/Application.java
@@ -0,0 +1,14 @@
+package de.wkss.addisonpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Artur.Zeiler on 10.05.2016.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String...args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
